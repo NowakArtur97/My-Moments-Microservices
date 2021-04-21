3135115679a103d44d68ccc9eea74c5b125f9b9b
NowakArtur97 <nowart97@gmail.com>
2021-04-21 09:37:11 +0200

    Register User Service and add Mongo DB service to docker compose file

--- user-service/src/main/java/com/nowakArtur97/myMoments/userService/UserServiceApplication.java
+++ user-service/src/main/java/com/nowakArtur97/myMoments/userService/UserServiceApplication.java
@@ -2,12 +2,14 @@ package com.nowakArtur97.myMoments.userService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 
 }
