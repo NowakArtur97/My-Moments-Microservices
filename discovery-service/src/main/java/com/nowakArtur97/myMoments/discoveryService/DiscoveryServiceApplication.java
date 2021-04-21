eb6a757dc4bc0a10277e1a27324d31fc55e1abda
NowakArtur97 <nowart97@gmail.com>
2021-04-21 08:12:36 +0200

    Generate Discovery Service using Spring Initializr

---
+++ discovery-service/src/main/java/com/nowakArtur97/myMoments/discoveryService/DiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nowakArtur97.myMoments.discoveryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ discovery-service/src/test/java/com/nowakArtur97/myMoments/discoveryService/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowakArtur97.myMoments.discoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
