b112cb78f616d185d12e68f38625c4eb8aef4157
NowakArtur97 <nowart97@gmail.com>
2021-04-22 07:53:00 +0200

    Create Jwt Request Filter and configure Web Security

---
+++ user-service/src/main/java/com/nowakArtur97/myMoments/userService/configuration/security/JwtRequestFilter.java
@@ -0,0 +1,79 @@
+package com.nowakArtur97.myMoments.userService.configuration.security;
+
+import com.nowakArtur97.myMoments.userService.common.util.JwtUtil;
+import com.nowakArtur97.myMoments.userService.exception.JwtTokenMissingException;
+import com.nowakArtur97.myMoments.userService.feature.user.document.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@EnableConfigurationProperties(value = JwtConfigurationProperties.class)
+class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    private final JwtUtil jwtUtil;
+
+    private final JwtConfigurationProperties jwtConfigurationProperties;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String authorizationHeader = request.getHeader(jwtConfigurationProperties.getAuthorizationHeader());
+
+        String username;
+        String jwt;
+
+        if (isBearerTypeAuthorization(authorizationHeader)) {
+
+            jwt = authorizationHeader.substring(jwtConfigurationProperties.getAuthorizationHeaderLength());
+            username = jwtUtil.extractUsername(jwt);
+        } else {
+            throw new JwtTokenMissingException("JWT token is missing in request headers.");
+        }
+
+        if (SecurityContextHolder.getContext().getAuthentication() == null && username != null) {
+
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+
+            if (jwtUtil.isTokenValid(jwt, userDetails)) {
+
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken
+                        = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+
+        String path = request.getRequestURI().substring(request.getContextPath().length());
+
+        return jwtConfigurationProperties.getIgnoredEndpoints().stream().anyMatch(path::contains);
+    }
+
+    private boolean isBearerTypeAuthorization(String authorizationHeader) {
+
+        return authorizationHeader != null && authorizationHeader.startsWith(jwtConfigurationProperties.getAuthorizationType());
+    }
+}
---
+++ user-service/src/main/java/com/nowakArtur97/myMoments/userService/configuration/security/WebSecurityConfiguration.java
@@ -0,0 +1,85 @@
+package com.nowakArtur97.myMoments.userService.configuration.security;
+
+import com.nowakArtur97.myMoments.userService.feature.user.document.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableConfigurationProperties(value = JwtConfigurationProperties.class)
+class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    private final JwtConfigurationProperties jwtConfigurationProperties;
+
+    private final JwtRequestFilter jwtRequestFilter;
+
+    private final ExceptionHandlerFilter exceptionHandlerFilter;
+
+    @Bean
+    PasswordEncoder getBCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationProvider getDaoAuthenticationProvider() {
+
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+
+        daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(getBCryptPasswordEncoder());
+
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(jwtConfigurationProperties.getIgnoredAntMatchers().toArray(new String[0]))
+                .permitAll()
+                .antMatchers(jwtConfigurationProperties.getAuthenticatedAntMatchers().toArray(new String[0]))
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.addFilterBefore(exceptionHandlerFilter, CorsFilter.class);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(customUserDetailsService);
+    }
+}
