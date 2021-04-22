a6df0b1214280abc91ed5fdf203da564e2435aa8
NowakArtur97 <nowart97@gmail.com>
2021-04-22 07:22:36 +0200

    Create Swagger Configuration and Properties

---
+++ user-service/src/main/java/com/nowakArtur97/myMoments/userService/configuration/swagger/SwaggerConfigurationProperties.java
@@ -0,0 +1,35 @@
+package com.nowakArtur97.myMoments.userService.configuration.swagger;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@ConfigurationProperties(prefix = "my-moments.swagger")
+@ConstructorBinding
+@AllArgsConstructor
+@Getter
+final class SwaggerConfigurationProperties {
+
+    private final String version;
+
+    private final String title;
+
+    private final String description;
+
+    private final String termsOfServiceUrl;
+
+    private final String license;
+
+    private final String licenseUrl;
+
+    private final String contactName;
+
+    private final String contactEmail;
+
+    private final String contactUrl;
+
+    private final String pathSelectors;
+
+    private final String authorizationHeader;
+}
