220370e548d47bef83347fc5b9eda1b70ff2a23e
NowakArtur97 <nowart97@gmail.com>
2021-04-26 07:31:42 +0200

    Setup applicatiion properties and enable Eureka Client in Post Service

--- post-service/src/main/java/com/nowakArtur97/myMoments/postService/PostServiceApplication.java
+++ post-service/src/main/java/com/nowakArtur97/myMoments/postService/PostServiceApplication.java
@@ -2,12 +2,16 @@ package com.nowakArtur97.myMoments.postService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableMongoAuditing
 public class PostServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PostServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PostServiceApplication.class, args);
+    }
 
 }
