4ac9c3262ce058c2357bb2a7a991c034912b5949
NowakArtur97 <nowart97@gmail.com>
2021-04-26 07:27:34 +0200

    Generate Post Service app using Spring Initializr

---
+++ post-service/src/main/java/com/nowakArtur97/myMoments/postService/PostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nowakArtur97.myMoments.postService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostServiceApplication.class, args);
+	}
+
+}
