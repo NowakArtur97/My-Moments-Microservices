ac8fd8827100decd45e41bf691554abb18804fec
NowakArtur97 <nowart97@gmail.com>
2021-04-27 10:55:26 +0200

    Create Controller Advices

---
+++ post-service/src/main/java/com/nowakArtur97/myMoments/postService/advice/AuthenticationControllerAdvice.java
@@ -0,0 +1,64 @@
+package com.nowakArtur97.myMoments.postService.advice;
+
+import com.nowakArtur97.myMoments.postService.common.model.ErrorResponse;
+import com.nowakArtur97.myMoments.postService.exception.JwtTokenMissingException;
+import io.jsonwebtoken.JwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestControllerAdvice(basePackages = "com.nowakArtur97.myMoments.postService.feature")
+public class AuthenticationControllerAdvice {
+
+    @ExceptionHandler({BadCredentialsException.class})
+    ResponseEntity<ErrorResponse> handleBadCredentialsException(BadCredentialsException exception) {
+
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), HttpStatus.UNAUTHORIZED.value(),
+                List.of("Invalid login credentials."));
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler({UsernameNotFoundException.class})
+    ResponseEntity<ErrorResponse> handleUsernameNotFoundException(UsernameNotFoundException exception) {
+
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), HttpStatus.UNAUTHORIZED.value(),
+                List.of(exception.getMessage()));
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler({JwtTokenMissingException.class})
+    ResponseEntity<ErrorResponse> handleJwtTokenMissingException(JwtTokenMissingException exception) {
+
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), HttpStatus.UNAUTHORIZED.value(),
+                List.of(exception.getMessage()));
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler({AuthenticationException.class})
+    ResponseEntity<ErrorResponse> handleJwtTokenMissingException(AuthenticationException exception) {
+
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), HttpStatus.UNAUTHORIZED.value(),
+                List.of(exception.getMessage()));
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler({JwtException.class})
+    ResponseEntity<ErrorResponse> handleJwtException(JwtException exception) {
+
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(), HttpStatus.UNAUTHORIZED.value(),
+                List.of(exception.getMessage()));
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ post-service/src/main/java/com/nowakArtur97/myMoments/postService/common/model/ErrorResponse.java
@@ -0,0 +1,43 @@
+package com.nowakArtur97.myMoments.postService.common.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+public class ErrorResponse {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
+    private final LocalDateTime timestamp;
+
+    private final int status;
+
+    private final List<String> errors;
+
+    public ErrorResponse(LocalDateTime timestamp, int status, List<String> errors) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.errors = errors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (!(o instanceof ErrorResponse)) return false;
+
+        ErrorResponse that = (ErrorResponse) o;
+
+        return getStatus() == that.getStatus() &&
+                Objects.equals(getTimestamp(), that.getTimestamp()) &&
+                Objects.equals(getErrors(), that.getErrors());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTimestamp(), getStatus(), getErrors());
+    }
+}
