8eb6def1a4fa10bb25a1832470704a5b2c31d3ee
NowakArtur97 <nowart97@gmail.com>
2021-04-27 09:11:05 +0200

    Get Roles with User in Post Service

--- post-service/src/main/java/com/nowakArtur97/myMoments/postService/feature/user/document/UserDocument.java
+++ post-service/src/main/java/com/nowakArtur97/myMoments/postService/feature/user/document/UserDocument.java
@@ -7,6 +7,9 @@ import lombok.ToString;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Document(collection = "user")
 @Getter
 @Setter
@@ -21,10 +24,27 @@ public class UserDocument extends AbstractDocument {
 
     private String password;
 
-    public UserDocument(String username, String email, String password) {
+    private Set<RoleDocument> roles;
+
+    public void addRole(RoleDocument role) {
+
+        this.getRoles().add(role);
+    }
+
+    public void removeRole(RoleDocument role) {
+
+        this.getRoles().remove(role);
+    }
+
+    public UserDocument() {
+        this.roles = new HashSet<>();
+    }
+
+    public UserDocument(String username, String email, String password, Set<RoleDocument> roles) {
 
         this.username = username;
         this.email = email;
         this.password = password;
+        this.roles = roles;
     }
 }
