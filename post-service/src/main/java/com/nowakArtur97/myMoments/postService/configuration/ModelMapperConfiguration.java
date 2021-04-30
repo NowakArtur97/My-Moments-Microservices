ee6327fd36f922cfe2e187d4d838f063c0c491d5
NowakArtur97 <nowart97@gmail.com>
2021-04-30 08:14:06 +0200

    Add Model Mapper dependency and Configuration

---
+++ post-service/src/main/java/com/nowakArtur97/myMoments/postService/configuration/ModelMapperConfiguration.java
@@ -0,0 +1,23 @@
+package com.nowakArtur97.myMoments.postService.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class ModelMapperConfiguration {
+
+    @Bean
+    ModelMapper getModelMapper() {
+
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.LOOSE)
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
+
+        return modelMapper;
+    }
+}
