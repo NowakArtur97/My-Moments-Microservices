92cbce9a8c37037674ae0214e0a9cd6abde9d25d
NowakArtur97 <nowart97@gmail.com>
2021-05-08 08:34:21 +0200

    Generate Comment Service

---
+++ comment-service/src/test/java/com/nowakArtur97/myMoments/commentService/CommentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowakArtur97.myMoments.commentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
