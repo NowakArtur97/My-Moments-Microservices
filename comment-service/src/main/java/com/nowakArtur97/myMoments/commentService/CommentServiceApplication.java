86383eb326c0666f5a54643564793b0e7bca2313
NowakArtur97 <nowart97@gmail.com>
2021-05-08 08:40:57 +0200

    Configure Eureka Client and paste basic properties

--- comment-service/src/main/java/com/nowakArtur97/myMoments/commentService/CommentServiceApplication.java
+++ comment-service/src/main/java/com/nowakArtur97/myMoments/commentService/CommentServiceApplication.java
@@ -2,12 +2,16 @@ package com.nowakArtur97.myMoments.commentService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableReactiveMongoAuditing
 public class CommentServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CommentServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CommentServiceApplication.class, args);
+    }
 
 }
