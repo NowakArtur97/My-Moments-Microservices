0e7a8beabdb6b8ace8ea423c1c6cd93b859dc898
NowakArtur97 <nowart97@gmail.com>
2021-05-13 11:26:21 +0200

    Generate Configuration Service

---
+++ configuration-service/src/main/java/com/nowakArtur97/myMoments/configurationService/ConfigurationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nowakArtur97.myMoments.configurationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationServiceApplication.class, args);
+	}
+
+}
---
+++ configuration-service/src/test/java/com/nowakArtur97/myMoments/configurationService/ConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowakArtur97.myMoments.configurationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
