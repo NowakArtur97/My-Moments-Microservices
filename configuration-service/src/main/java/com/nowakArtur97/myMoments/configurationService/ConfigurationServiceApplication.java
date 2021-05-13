e49b4ee4a7a53fcfd746de09f46d70febd26749e
NowakArtur97 <nowart97@gmail.com>
2021-05-13 11:37:47 +0200

    Enable Config Server and update .gitignore file

--- configuration-service/src/main/java/com/nowakArtur97/myMoments/configurationService/ConfigurationServiceApplication.java
+++ configuration-service/src/main/java/com/nowakArtur97/myMoments/configurationService/ConfigurationServiceApplication.java
@@ -2,12 +2,14 @@ package com.nowakArtur97.myMoments.configurationService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigurationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigurationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationServiceApplication.class, args);
+    }
 
 }
