d9bcb6ae5cd77363d3e59cafd803e10b93d72eb0
NowakArtur97 <nowart97@gmail.com>
2021-05-14 09:39:22 +0200

    Try to fix issue with RabbitMQ properties for services

--- post-service/src/main/java/com/nowakArtur97/myMoments/postService/feature/post/UserEventConsumer.java
+++ post-service/src/main/java/com/nowakArtur97/myMoments/postService/feature/post/UserEventConsumer.java
@@ -1,12 +1,14 @@
 package com.nowakArtur97.myMoments.postService.feature.post;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 class UserEventConsumer {
 
     private final PostRepository postRepository;
@@ -16,6 +18,8 @@ class UserEventConsumer {
 
         UserUpdateEventPayload payload = message.getPayload();
         String newUsername = payload.getNewUsername();
+
+        log.info(newUsername);
 
         postRepository.findByAuthor(payload.getPreviousUsername())
                 .doOnNext(postDocument -> postDocument.setAuthor(newUsername))
@@ -27,6 +31,8 @@ class UserEventConsumer {
     public void onUserDeleteMessage(Message<String> message) {
 
         String usernamePayload = message.getPayload();
+
+        log.info(usernamePayload);
 
         postRepository.deleteByAuthor(usernamePayload)
                 .subscribe();
