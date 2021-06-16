9b44df3d398fa16c4ee4d2ef169e3dc5d2e3687c
NowakArtur97 <nowart97@gmail.com>
2021-06-16 11:04:20 +0200

    Fix issue with getting values from Vault

--- vault-client/entrypoint/vault-init.sh
+++ vault-client/entrypoint/vault-init.sh
@@ -11,12 +11,12 @@ echo "Authenticating to vault..."
 vault login token=vault-plaintext-root-token
 
 echo "Initializing vault..."
-vault secrets enable -version=2 -path=configuration-service kv
+vault secrets enable -version=2 -path=/secret/application kv
 
 echo "Adding entries..."
-vault kv put -format=json /secret/configuration-service @/vault/file/configuration-service.json
+vault kv put -format=json /secret/application @/vault/file/configuration-service.json
 
 echo "Retrieving entries..."
-vault kv get secret/configuration-service
+vault kv get secret/application
 
 echo "Complete..."
