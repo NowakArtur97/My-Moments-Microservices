53b217810249e4a0fa01d27d6af1fb891101c46f
NowakArtur97 <nowart97@gmail.com>
2021-09-04 16:29:09 +0200

    Generate Follower Service using Spring Initilizr

---
+++ follower-service/src/main/java/com/nowakArtur97/myMoments/followerService/FollowerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nowakArtur97.myMoments.followerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowerServiceApplication.class, args);
+	}
+
+}
