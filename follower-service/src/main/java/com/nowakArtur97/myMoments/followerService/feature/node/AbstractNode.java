23d514e48cb02b34b1cf42b5e5ace2b41ff2d266
NowakArtur97 <nowart97@gmail.com>
2021-09-06 15:27:16 +0200

    Create Abstract Node in Follower Service

---
+++ follower-service/src/main/java/com/nowakArtur97/myMoments/followerService/feature/node/AbstractNode.java
@@ -0,0 +1,34 @@
+package com.nowakArtur97.myMoments.followerService.feature.node;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.neo4j.core.schema.Node;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Node
+@EqualsAndHashCode(of = "uuid")
+@Getter
+@Setter(AccessLevel.PRIVATE)
+@ToString
+abstract class AbstractNode {
+
+    @Id
+    @Setter
+    private String id;
+
+    private String uuid = UUID.randomUUID().toString();
+
+    @CreatedDate
+    private Date createDate;
+
+    @LastModifiedDate
+    private Date modifyDate;
+
+    @Version
+    private Integer version;
+}
