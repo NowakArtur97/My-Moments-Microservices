a1157b94a2283c5d884b7c3b3c26b1b2d5e00cee
NowakArtur97 <nowart97@gmail.com>
2021-09-09 16:28:54 +0200

    Configure messages for data Validation

---
+++ follower-service/src/main/java/com/nowakArtur97/myMoments/followerService/configuration/ValidationMessagesSourceConfiguration.java
@@ -0,0 +1,34 @@
+package com.nowakArtur97.myMoments.followerService.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+class ValidationMessagesSourceConfiguration {
+
+    @Bean
+    @Primary
+    LocalValidatorFactoryBean getValidator() {
+
+        LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+
+        localValidatorFactoryBean.setValidationMessageSource(getMessageSource());
+
+        return localValidatorFactoryBean;
+    }
+
+    @Bean
+    MessageSource getMessageSource() {
+
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath:/validation/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+
+        return messageSource;
+    }
+}
