e733d4144610b2a51564e4ed576e88dceae2deea
NowakArtur97 <nowart97@gmail.com>
2022-02-07 15:42:48 +0100

    Refresh form fields validation on change

--- client/src/app/auth/registration/registration.component.ts
+++ client/src/app/auth/registration/registration.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { AbstractControl, NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import UserRegistrationDTO from '../models/user-registration-dto.model';
 
@@ -8,9 +9,11 @@ import UserRegistrationDTO from '../models/user-registration-dto.model';
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css'],
 })
-export class RegistrationComponent implements OnInit {
-  @ViewChild('registrationForm', { static: true }) registrationForm!: NgForm;
+export class RegistrationComponent
+  implements OnInit, OnDestroy, AfterViewChecked {
+  @ViewChild('registrationForm') registrationForm!: NgForm;
 
+  private registerFormSubscriptions$ = new Subscription();
   userRegistrationDTO: UserRegistrationDTO = {
     username: '',
     email: '',
@@ -22,25 +25,50 @@ export class RegistrationComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  ngOnDestroy = (): void => this.registerFormSubscriptions$?.unsubscribe();
+
+  ngAfterViewChecked = (): void => this.refreshFormFieldsAfterChange();
+
   onRegister(): void {
     if (this.registrationForm.controls['matching_password'].errors) {
       console.log(this.password.errors);
     }
   }
 
+  private refreshFormFieldsAfterChange() {
+    if (this.username) {
+      this.registerFormSubscriptions$.add(
+        this.username.valueChanges.subscribe(() => {
+          this.password.updateValueAndValidity({ emitEvent: false });
+          this.matchingPassword.updateValueAndValidity({ emitEvent: false });
+        })
+      );
+      this.registerFormSubscriptions$.add(
+        this.password.valueChanges.subscribe(() => {
+          this.matchingPassword.updateValueAndValidity({ emitEvent: false });
+        })
+      );
+      this.registerFormSubscriptions$.add(
+        this.matchingPassword.valueChanges.subscribe(() => {
+          this.password.updateValueAndValidity({ emitEvent: false });
+        })
+      );
+    }
+  }
+
   get username(): AbstractControl {
-    return this.registrationForm.controls['username'];
+    return this.registrationForm?.controls['username'];
   }
 
   get email(): AbstractControl {
-    return this.registrationForm.controls['email'];
+    return this.registrationForm?.controls['email'];
   }
 
   get password(): AbstractControl {
-    return this.registrationForm.controls['password'];
+    return this.registrationForm?.controls['password'];
   }
 
   get matchingPassword(): AbstractControl {
-    return this.registrationForm.controls['matchingPassword'];
+    return this.registrationForm?.controls['matching_password'];
   }
 }
