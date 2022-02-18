f693ca18f8e092b809bdadf6637d97dbf1981ef3
NowakArtur97 <nowart97@gmail.com>
2022-02-18 14:19:07 +0100

    Create Auth Wrapper component

---
+++ client/src/app/auth/auth-wrapper/auth-wrapper.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-wrapper',
+  templateUrl: './auth-wrapper.component.html',
+  styleUrls: ['./auth-wrapper.component.css'],
+})
+export class AuthWrapperComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppCommonModule } from '../common/common.module';
+import { AuthWrapperComponent } from './auth-wrapper/auth-wrapper.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { PasswordRulesDirective } from './directives/password-rules.directive';
 import { RegistrationComponent } from './registration/registration.component';
@@ -13,8 +14,9 @@ import { RegistrationComponent } from './registration/registration.component';
     RegistrationComponent,
     PasswordRulesDirective,
     AuthenticationComponent,
+    AuthWrapperComponent,
   ],
   imports: [FormsModule, BrowserModule, HttpClientModule, AppCommonModule],
-  exports: [RegistrationComponent, AuthenticationComponent],
+  exports: [AuthWrapperComponent],
 })
 export class AuthModule {}
