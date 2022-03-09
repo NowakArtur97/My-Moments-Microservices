b83b0cf5363470e14f5ae7c607b5c72c6c2aef63
NowakArtur97 <nowart97@gmail.com>
2022-03-09 07:59:51 +0100

    Create infinite animation for Background

--- client/src/app/common/background/background.component.ts
+++ client/src/app/common/background/background.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { animate, AnimationEvent, state, style, transition, trigger } from '@angular/animations';
+import { AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
 
 import { BackgroundService } from './background.service';
 
@@ -6,8 +7,28 @@ import { BackgroundService } from './background.service';
   selector: 'app-background',
   templateUrl: './background.component.html',
   styleUrls: ['./background.component.css'],
+  animations: [
+    trigger('move', [
+      state(
+        'down',
+        style({
+          transform: 'translateY(-100px)',
+        })
+      ),
+      state(
+        'up',
+        style({
+          transform: 'translateY(100px)',
+        })
+      ),
+      transition('down => up', animate('300ms linear')),
+      transition('up => down', animate('300ms linear')),
+    ]),
+  ],
 })
-export class BackgroundComponent implements OnInit {
+export class BackgroundComponent implements OnInit, AfterViewInit {
+  private readonly TILES_ANIMATIONS_STATES = { UP: 'up', DOWN: 'down' };
+  state = this.TILES_ANIMATIONS_STATES.UP;
   tiles!: String[][];
   tileHeight!: String;
   gridColumns!: String;
@@ -22,6 +43,20 @@ export class BackgroundComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.state = this.TILES_ANIMATIONS_STATES.DOWN;
+    }, 0);
+  }
+  onEnd(event: AnimationEvent) {
+    this.state = this.TILES_ANIMATIONS_STATES.UP;
+    if (event.toState === this.TILES_ANIMATIONS_STATES.UP) {
+      setTimeout(() => {
+        this.state = this.TILES_ANIMATIONS_STATES.DOWN;
+      }, 0);
+    }
+  }
 
   setupTiles() {
     this.tiles = this.backgroundService.getRandomImages();
