6a8f7ab9347f92c0e50f049d22fadf693f1811d8
NowakArtur97 <nowart97@gmail.com>
2022-03-09 08:37:05 +0100

    Add random timing to Background animation

--- client/src/app/common/background/background.component.ts
+++ client/src/app/common/background/background.component.ts
@@ -23,18 +23,25 @@ import { BackgroundService } from './background.service';
         }),
         { params: { tilesTopOffset: '0' } }
       ),
-      transition('down => up', animate('3000ms')),
-      transition('up => down', animate('3000ms')),
+      transition('down => up', animate('{{randomTiming}}ms'), {
+        params: { randomTiming: '0' },
+      }),
+      transition('up => down', animate('{{randomTiming}}ms'), {
+        params: { randomTiming: '0' },
+      }),
     ]),
   ],
 })
 export class BackgroundComponent implements OnInit, AfterViewInit {
   private readonly TILES_ANIMATIONS_STATES = { UP: 'up', DOWN: 'down' };
-  tilesTopOffset!: String;
+  private readonly ANIMATIONS_TIMINGS = { MIN: 1000, MAX: 3000 };
+
   state = this.TILES_ANIMATIONS_STATES.UP;
   tiles!: String[][];
   tileHeight!: String;
   gridColumns!: String;
+  tilesTopOffset!: String;
+  randomTiming!: number;
 
   @HostListener('window:resize', ['$event'])
   private onResize(): void {
@@ -55,8 +62,8 @@ export class BackgroundComponent implements OnInit, AfterViewInit {
       tileHeight *
       (this.backgroundService.NUMBER_OF_BACKUP_IMAGES_FOR_ANIMATION / 2)
     }px`;
-    console.log(this.tilesTopOffset);
     this.gridColumns = `repeat(${this.tiles.length}, 1fr)`;
+    this.randomTiming = this.generateRandomTiming();
   }
 
   ngAfterViewInit(): void {
@@ -67,10 +74,18 @@ export class BackgroundComponent implements OnInit, AfterViewInit {
 
   onEnd(event: AnimationEvent) {
     this.state = this.TILES_ANIMATIONS_STATES.UP;
+    this.randomTiming = this.generateRandomTiming();
     if (event.toState === this.TILES_ANIMATIONS_STATES.UP) {
       setTimeout(() => {
         this.state = this.TILES_ANIMATIONS_STATES.DOWN;
       }, 0);
     }
   }
+
+  private generateRandomTiming = (): number =>
+    Math.floor(
+      Math.random() *
+        (this.ANIMATIONS_TIMINGS.MAX - this.ANIMATIONS_TIMINGS.MIN + 1) +
+        this.ANIMATIONS_TIMINGS.MIN
+    );
 }
