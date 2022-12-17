7dd7bcdb7732f699b9a7ee8d9599cd4b6c251a86
NowakArtur97 <nowart97@gmail.com>
2022-12-17 13:39:34 +0100

    Setup Comment Service on Docker and CORS config for front

--- client/src/environments/environment.local.ts
+++ client/src/environments/environment.local.ts
@@ -4,4 +4,5 @@ const localhostUrl = 'http://localhost:';
 export const environment = {
   userServiceUrl: `${baseUrl}8081/api/v1`,
   postServiceUrl: `${localhostUrl}8082/api/v1/posts`,
+  commentsServiceUrl: `${baseUrl}8083/api/v1/posts`,
 };
