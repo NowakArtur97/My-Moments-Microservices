cfc24bb1d93046165b8cfde08c431aa23cde0f4d
NowakArtur97 <nowart97@gmail.com>
2023-01-04 08:35:25 +0100

    Create directive for stopping propagation of event on click

--- client/src/app/comment/comments.module.ts
+++ client/src/app/comment/comments.module.ts
@@ -1,13 +1,14 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { AppCommonModule } from '../common/common.module';
 import { CommentEditComponent } from './comment-edit/comment-edit.component';
 import { CommentComponent } from './comment/comment.component';
 import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   declarations: [CommentsComponent, CommentComponent, CommentEditComponent],
-  imports: [CommonModule],
+  imports: [FormsModule, AppCommonModule],
   exports: [CommentsComponent],
 })
 export class CommentsModule {}
--- client/src/app/common/common.module.ts
+++ client/src/app/common/common.module.ts
@@ -9,6 +9,7 @@ import { BackgroundComponent } from './background/background.component';
 import { MustMatchDirective } from './directives/must-match.directive';
 import { NoWhiteSpacesDirective } from './directives/no-white-spaces.directive';
 import { NotIncludeDirective } from './directives/not-include.directive';
+import { OnClickStopPropagation } from './directives/on-click-stop-propagation.directive';
 
 const validationDirectives = [
   NotIncludeDirective,
@@ -19,6 +20,7 @@ const validationDirectives = [
 @NgModule({
   declarations: [
     validationDirectives,
+    OnClickStopPropagation,
     BackgroundComponent,
     BackgroundTilesComponent,
   ],
@@ -30,6 +32,7 @@ const validationDirectives = [
   ],
   exports: [
     validationDirectives,
+    OnClickStopPropagation,
     CommonModule,
     HttpClientModule,
     BackgroundComponent,
