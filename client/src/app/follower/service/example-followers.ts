08e24fe6e26fa601b171d25e61f27bc26f5d81c4
NowakArtur97 <nowart97@gmail.com>
2023-04-24 16:48:10 +0200

    Add media queries to Followers component

--- client/src/app/follower/service/example-followers.ts
+++ client/src/app/follower/service/example-followers.ts
@@ -1,26 +1,113 @@
 import Follower from '../models/follower.model';
 
+const MIN_FOLLOWERS = 5;
+const MAX_FOLLOWERS = 2000;
+
+const getRandomNumberOfUsers = (): number =>
+  Math.floor(Math.random() * (MAX_FOLLOWERS - MIN_FOLLOWERS + 1)) +
+  MIN_FOLLOWERS;
+
 const EXAMPLE_FOLLOWERS: Follower[] = [
-  { username: 'follower1', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower2', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower3', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower4', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower5', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower6', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower7', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower8', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower9', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower10', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower11', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower12', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower13', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower14', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower15', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower16', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower17', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower18', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower19', numberOfFollowers: 20, numberOfFollowing: 200 },
-  { username: 'follower20', numberOfFollowers: 20, numberOfFollowing: 200 },
+  {
+    username: 'follower1',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower2',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower3',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower4',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower5',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower6',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower7',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower8',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower9',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower10',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower11',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower12',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower13',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower14',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower15',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower16',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower17',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower18',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower19',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
+  {
+    username: 'follower20',
+    numberOfFollowers: getRandomNumberOfUsers(),
+    numberOfFollowing: getRandomNumberOfUsers(),
+  },
 ];
 
 export default EXAMPLE_FOLLOWERS;
