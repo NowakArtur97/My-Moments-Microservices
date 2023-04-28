a20b2489060e2c2117e7f42ad34d28d191fc7beb
NowakArtur97 <nowart97@gmail.com>
2023-04-28 08:51:27 +0200

    Create animations for follow back and unfollow User buttons

--- client/src/app/follower/user-acquaintance/user-acquaintance.component.ts
+++ client/src/app/follower/user-acquaintance/user-acquaintance.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 
 import UserAcquaintance from '../models/user-acquaintance.model';
 
@@ -6,11 +7,61 @@ import UserAcquaintance from '../models/user-acquaintance.model';
   selector: 'app-user-acquaintance',
   templateUrl: './user-acquaintance.component.html',
   styleUrls: ['./user-acquaintance.component.css'],
+  animations: [
+    trigger('state', [
+      state(
+        'default',
+        style({
+          transform: 'translateY(0%)',
+        })
+      ),
+      state(
+        'hover',
+        style({
+          transform: 'translateY(-100%)',
+        })
+      ),
+      transition('default <=> hover', animate('0.5s')),
+    ]),
+    trigger('buttonState', [
+      state(
+        'default',
+        style({
+          transform: 'translateY(-100%)',
+        })
+      ),
+      state(
+        'hover',
+        style({
+          transform: 'translateY(0%)',
+        })
+      ),
+      transition('default <=> hover', animate('0.5s')),
+    ]),
+  ],
 })
 export class UserAcquaintanceComponent implements OnInit {
   @Input('user') user!: UserAcquaintance;
 
+  isHovered = false;
+  state = 'default';
+  buttonState = 'default';
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  @HostListener('mouseover')
+  onHover(): void {
+    this.isHovered = true;
+    this.state = 'hover';
+    this.buttonState = 'hover';
+  }
+
+  @HostListener('mouseout')
+  onLeave(): void {
+    this.isHovered = false;
+    this.state = 'default';
+    this.buttonState = 'default';
+  }
 }
